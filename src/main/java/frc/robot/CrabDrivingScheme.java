439c51082504d6f829eb7b18c45fab9e3bf3be28
Uche Akotaobi <uche.robotics@akotaobi.org>
2022-01-26 15:33:03 -0800

    Made a little progress on the CrabDrivingScheme.
    Not much, though: we still need to call atan2() to convert those vectors into
    swerve module angles -- and then get the speeds from a side channel, like the
    length of the original absoluteTranslationVector.

--- src/main/java/frc/robot/CrabDrivingScheme.java
+++ src/main/java/frc/robot/CrabDrivingScheme.java
@@ -6,6 +6,7 @@ import java.util.List;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /**
  * An alternative {@link frc.robot.DrivingScheme DrivingScheme} based on vector
@@ -18,17 +19,51 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
  */
 public class CrabDrivingScheme implements DrivingScheme {
 
+    private Gyro gyro = null;
+
+    /**
+     *
+     * @param gyro A {@link Gyro} object which is initialized such that the
+     * front of the robot points away from the drivers when the gyro angle is 0.
+     * Being consistent with this is very important for getting field-oriented
+     * swerve working -- if the robot does not face forward when the gyro is
+     * initialized, it will drive incorrectly for the remainder of the match.
+     */
+    CrabDrivingScheme(Gyro gyro) {
+        this.gyro = gyro;
+    }
+
     @Override
     public SwerveModuleState[] convert(double frontBack, double leftRight, double rotation) {
+
+        // Clamp the input values to the range [-1, 1].
+        frontBack = Math.min(1.0, Math.max(-1.0, frontBack));
+        leftRight = Math.min(1.0, Math.max(-1.0, leftRight));
+        rotation = Math.min(1.0, Math.max(-1.0, rotation));
+
+        // This is where the driver wants to go with respect to the field's
+        // reference frame.
         Translation2d absoluteTranslationVector = new Translation2d(leftRight, frontBack);
+
+        // This is where the robot should be going with respect to ITS OWN
+        // reference frame.  This is why we need the gyro.
+        //
+        // The WPILib Gyro class returns larger angles as the robot rotates
+        // clockwise with respect to the top of the chassis.  Since we require
+        // the robot to alway starts the match facing away from the driver's
+        // side of the field, rotating the chassis by -gyro.getAngle() will
+        // always face it in its starting direction.
+        //
+        // Rotate the joystick vector by the same amount.
+        Translation2d adjustedTranslationVector = absoluteTranslationVector.rotateBy(gyro.getRotation2d());
 
         // Define the direction vectors for all four swerve wheels. Initially,
         // they start as copies of the overall direction vector.
         Translation2d[] swerveTranslationVectors = new Translation2d[] {
-            absoluteTranslationVector,
-            absoluteTranslationVector,
-            absoluteTranslationVector,
-            absoluteTranslationVector
+            adjustedTranslationVector,
+            adjustedTranslationVector,
+            adjustedTranslationVector,
+            adjustedTranslationVector
         };
 
         // The "crab rotation" vectors are the four vectors that are tangent to
@@ -73,12 +108,24 @@ public class CrabDrivingScheme implements DrivingScheme {
 
         // Translate the crab rotation angles into vectors.
         Translation2d[] crabRotationVectors = { null, null, null, null };
+        Translation2d[] finalVectors = { null ,null, null, null };
+        final double r = Math.abs(rotation);
         for (int i = 0; i < thetas.length; ++i) {
-            // We assume here that the up vector faces forward.  The rotation
-            // here is counterclockwise around the origin.
-            double angleRadians = thetas[i] > 0 ? thetas[i] : Math.PI + thetas[i];
+
+            // Negative angles should rotate counterclockwise.
+            double angleRadians = rotation > 0 ? thetas[i] : Math.PI + thetas[i];
+
+            // We assume here that up vectors [Translate2d(0, -1)] point forward
+            // with respect to the robot chassis.  The rotateBy() calls here
+            // rotate counterclockwise around the origin.
             crabRotationVectors[i] = new Translation2d(0, -1).rotateBy(new Rotation2d(angleRadians));
+
+            // Use the rotation channel to combine the translation and rotation
+            // vectors.
+            finalVectors[i] = adjustedTranslationVector.times(1 - r).plus(crabRotationVectors[i].times(r));
         }
+
+        // Convert the vectors into SwerveModuleStates.
         return null;
     }
 
