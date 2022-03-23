91eb341a865be7268d2ec462658ff3cb1c7bf02a
Xmassub <mbusse_24@esusdstudents.org>
2022-03-22 17:44:45 -0700

    Staging stanges to merge.

--- src/main/java/frc/robot/drivers/SparkMaxSwerveDriver.java
+++ src/main/java/frc/robot/drivers/SparkMaxSwerveDriver.java
@@ -5,16 +5,25 @@ import java.util.Collections;
 import java.util.List;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.Constants;
 
 public class SparkMaxSwerveDriver implements SwerveDriver {
     /**
+     * Stores values so we can see the values of delta degrees
+     */
+    private List<NetworkTableEntry> entries;
+
+     /**
      * Controls the speed of the four swerve wheels.
      */
     private List<CANSparkMax> speedMotors;
@@ -24,6 +33,7 @@ public class SparkMaxSwerveDriver implements SwerveDriver {
      * Whenever the reversal flag is true for a motor, then we will reverse the
      * motor's direction in software.
      */
+    // How come you put "List" insted of "ArrayList", are they different? - Sam.
     private List<Boolean> reversalFlags;
 
     /**
@@ -34,15 +44,26 @@ public class SparkMaxSwerveDriver implements SwerveDriver {
     /**
      * Rotates the pivot motors to a given set point using PID.
      */
-    private List<SparkMaxPIDController> pidControllers;
+    private List<PIDController> pidControllers;
 
-    private final double P = 0.1;
-    private final double I = 1e-4;
-    private final double D = 1;
-    private final double Iz = 0;
-    private final double FF = 0;
-    private final double MAX_OUTPUT = 1;
-    private final double MIN_OUTPUT = -1;
+    /**
+     * Converts inputs from PWM into dutyCycles between zero and one.
+     */
+    private List<DutyCycle> dutyCycles;
+
+    /**
+     * These angles and speeds is where our drive "wants" to end up.
+     * But it still won't use them unless you call drive(getGoalStates()).
+     */
+    private SwerveModuleState[] goalStates;
+
+    // Manually-determined PID for the pivot motors.
+    //
+    // It took a long time to come up with these values.  Are you a bad enough dude
+    // to modify them?
+    private final double P = 1.0;
+    private final double I = 1.0;
+    private final double D = 0.01;
 
     /**
      * Initializes all of the {@link CANSparkMax} motor and PID controllers.
@@ -50,12 +71,20 @@ public class SparkMaxSwerveDriver implements SwerveDriver {
     public SparkMaxSwerveDriver() {
         // Initialize the swerve motors (pivot and speed.)
         try {
+            goalStates = reset(0);
             reversalFlags = new ArrayList<Boolean>();
             pivotMotors = new ArrayList<CANSparkMax>();
             speedMotors = new ArrayList<CANSparkMax>();
-            Collections.addAll(pivotMotors, new CANSparkMax[] { null, null, null, null });
-            Collections.addAll(speedMotors, new CANSparkMax [] { null, null, null, null });
-            Collections.addAll(reversalFlags, new Boolean[] { true, true, false, false });
+            dutyCycles = new ArrayList<DutyCycle>();
+            entries = new ArrayList<NetworkTableEntry>();
+            Collections.addAll(pivotMotors, new CANSparkMax[4]);
+            Collections.addAll(speedMotors, new CANSparkMax [4]);
+            Collections.addAll(reversalFlags, new Boolean[] { false, false, false, false });
+            // reversalFlags.set(Constants.FRONT_LEFT, true);
+            reversalFlags.set(Constants.FRONT_RIGHT, true);
+            reversalFlags.set(Constants.BACK_RIGHT, true);
+            Collections.addAll(dutyCycles, new DutyCycle[4]);
+            Collections.addAll(entries, new NetworkTableEntry[16]);
 
             speedMotors.set(Constants.FRONT_LEFT, new CANSparkMax(Constants.FRONT_LEFT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless));
             speedMotors.set(Constants.FRONT_RIGHT, new CANSparkMax(Constants.FRONT_RIGHT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless));
@@ -66,6 +95,11 @@ public class SparkMaxSwerveDriver implements SwerveDriver {
             pivotMotors.set(Constants.FRONT_RIGHT, new CANSparkMax(Constants.FRONT_RIGHT_TURN_MOTOR_CAN_ID, MotorType.kBrushless));
             pivotMotors.set(Constants.BACK_LEFT, new CANSparkMax(Constants.BACK_LEFT_TURN_MOTOR_CAN_ID, MotorType.kBrushless));
             pivotMotors.set(Constants.BACK_RIGHT, new CANSparkMax(Constants.BACK_RIGHT_TURN_MOTOR_CAN_ID, MotorType.kBrushless));
+
+            dutyCycles.set(Constants.FRONT_LEFT, new DutyCycle(new DigitalInput(Constants.FRONT_LEFT_ABSOLUTE_DIO_PORT)));
+            dutyCycles.set(Constants.BACK_LEFT, new DutyCycle(new DigitalInput(Constants.BACK_LEFT_ABSOLUTE_DIO_PORT)));
+            dutyCycles.set(Constants.BACK_RIGHT, new DutyCycle(new DigitalInput(Constants.BACK_RIGHT_ABSOLUTE_DIO_PORT)));
+            dutyCycles.set(Constants.FRONT_RIGHT, new DutyCycle(new DigitalInput(Constants.FRONT_RIGHT_ABSOLUTE_DIO_PORT)));
 
             pivotMotors.forEach(m -> {
                 // When we cut power, the motors should stop pivoting immediately;
@@ -78,25 +112,35 @@ public class SparkMaxSwerveDriver implements SwerveDriver {
                 m.getEncoder().setPositionConversionFactor(1 / Constants.WHEEL_TURN_RATIO);
             });
 
+            // Inverts drive motors based on reversal flags list
+            for (int i = 0; i < 4; i++) {
+                var m = speedMotors.get(i);
+                m.setInverted(reversalFlags.get(i));
+            }
+
         } catch (Throwable e) {
             System.out.printf("Exception thrown: %s", e.getMessage());
             e.printStackTrace();
         }
 
-        this.pidControllers = new ArrayList<SparkMaxPIDController>();
+        this.pidControllers = new ArrayList<PIDController>();
         int i = 0;
         for (CANSparkMax pivotMotor : pivotMotors) {
             try {
                 System.out.printf("about to get pidcontroller #%d\n", i++);
-                SparkMaxPIDController pidController = pivotMotor.getPIDController();
+                PIDController pidController = new PIDController(P, I, D);
+
+                // The pidController is supposed to minimize the deviation (error) between
+                // an aboslute angle, in degrees, and a setpoint coming from the human
+                // driver's controller, also in degrees.  To minimize this angle, we need
+                // to be aware that the range "wraps around" between 0 and 360 degrees.
+                pidController.enableContinuousInput(-180, 180);
 
                 // Set PID coefficients.
-                pidController.setP(0.1);
-                pidController.setI(1e-4);
-                pidController.setD(1);
-                pidController.setIZone(0);
-                pidController.setFF(0);
-                pidController.setOutputRange(MIN_OUTPUT, MAX_OUTPUT);
+                pidController.setP(P);
+                pidController.setI(I);
+                pidController.setD(D);
+
 
                 // Integrated encoder is the feedback device the controller uses by default.
                 // pidController.setFeedbackDevice(pivotMotor.getEncoder());
@@ -107,34 +151,107 @@ public class SparkMaxSwerveDriver implements SwerveDriver {
                 e.printStackTrace();
             }
         }
-        
+
         ShuffleboardTab shuffleboardTab = Shuffleboard.getTab("Drivetrain");
-        shuffleboardTab.addNumber("fLDefEnc", () -> pivotMotors.get(Constants.FRONT_LEFT).getEncoder().getPosition());
-        shuffleboardTab.addNumber("fRDefEnc", () -> pivotMotors.get(Constants.FRONT_RIGHT).getEncoder().getPosition());
-        shuffleboardTab.addNumber("bLDefEnc", () -> pivotMotors.get(Constants.BACK_LEFT).getEncoder().getPosition());
-        shuffleboardTab.addNumber("bRDefEnc", () -> pivotMotors.get(Constants.BACK_RIGHT).getEncoder().getPosition());
-        
         // shuffleboardTab.addNumber("fLAltEnc", () -> pivotMotors.get(Constants.FRONT_LEFT).getAlternateEncoder(Constants.SRX_MAG_ENCODER_CLICKS_PER_REVOLUTION).getPosition());
         // shuffleboardTab.addNumber("fRAltEnc", () -> pivotMotors.get(Constants.FRONT_RIGHT).getAlternateEncoder(Constants.SRX_MAG_ENCODER_CLICKS_PER_REVOLUTION).getPosition());
         // shuffleboardTab.addNumber("bLAltEnc", () -> pivotMotors.get(Constants.BACK_LEFT).getAlternateEncoder(Constants.SRX_MAG_ENCODER_CLICKS_PER_REVOLUTION).getPosition());
         // shuffleboardTab.addNumber("bRAltEnc", () -> pivotMotors.get(Constants.BACK_RIGHT).getAlternateEncoder(Constants.SRX_MAG_ENCODER_CLICKS_PER_REVOLUTION).getPosition());
+
+        for (int j = 0; j < 4; j++) {
+            String s = Constants.CORNER_NAME_ABBREVS[j];
+            entries.set(j + 0, shuffleboardTab.add(String.format("%s delta", s), 0).getEntry());
+            entries.set(j + 4, shuffleboardTab.add(String.format("%s angle", s), 0).getEntry());
+            entries.set(j + 8, shuffleboardTab.add(String.format("%s goal", s), 0).getEntry());
+            entries.set(j + 12, shuffleboardTab.add(String.format("%s speed", s), 0).getEntry());
+        }
+    }
+
+    /**
+     * Stores a new direction for the swerve modules to go in.
+     * You can pass these values into drive() using getGoalStates().
+     *
+     * @param newGoalStates New shopping cart angles and speeds that you want.
+     */
+    public void setGoalStates(SwerveModuleState[] newGoalStates) {
+        goalStates = newGoalStates;
+    }
+
+    /**
+     * Returns the goal states that were previously set by setGoalStates()
+     * so that you can pass those into drive().
+     * @return Returns the goal states that were passed into setGoalStates().
+     */
+    public SwerveModuleState[] getGoalStates() {
+        return goalStates;
+    }
+
+    /**
+     * Returns {@link SwerveModuleState SwerveModuleStates} where all four pivot wheels
+     * are aligned at the given absolute angle, regardless of where the the wheels were
+     * when the robot was turned on.
+     *
+     * @param absoluteAngleDegrees The desired angle for all the swerve modules.
+     *                             An angle of 0 points all modules forward, and is the
+     *                             correct position for teleopInit().  Positive values rotate
+     *                             clockwise and negative values rotate counterclockwise.
+     * @return The states.
+     */
+    public SwerveModuleState[] reset(double absoluteAngleDegrees) {
+        double absoluteAngleRadians = absoluteAngleDegrees * Math.PI/ 180;
+        return new SwerveModuleState[] {
+            new SwerveModuleState(absoluteAngleDegrees,
+                                  new Rotation2d(absoluteAngleRadians)),
+            new SwerveModuleState(absoluteAngleDegrees,
+                                  new Rotation2d(absoluteAngleRadians)),
+            new SwerveModuleState(absoluteAngleDegrees,
+                                  new Rotation2d(absoluteAngleRadians)),
+            new SwerveModuleState(absoluteAngleDegrees,
+                                  new Rotation2d(absoluteAngleRadians))
+        };
     }
 
     @Override
     public void drive(SwerveModuleState[] swerveModuleStates) {
+        ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
+
         // Translates shopping cart speeds and angles into motion.
-        for  (int i = 0; i < swerveModuleStates.length; i++)  {
+        for (int i = 0; i < swerveModuleStates.length; i++)  {
+
+            // Set the speed for the current drive motor.
+            // ------------------------------------------
 
             // Translate speed from units of meters per second into a unitless
             // value between -1.0 and 1.0.
             double speed = swerveModuleStates[i].speedMetersPerSecond /
                 Constants.ROBOT_MAXIMUM_SPEED_METERS_PER_SECOND;
             speedMotors.get(i).set(speed);
+            entries.get(i + 12).setDouble(speedMotors.get(i).get());
 
             // Set angle for current pivot motor.
-            double rotations = swerveModuleStates[i].angle.getDegrees() / 360;
-            pidControllers.get(i).setReference(rotations,
-                                               CANSparkMax.ControlType.kPosition);
+            // ----------------------------------
+
+            // This is the angle coming from the shopping cart angles in the swerveModuleStates[]
+            // argument.  The range is always [-180, 180].
+            double rotations = swerveModuleStates[i].angle.getDegrees() + 180.0;
+            entries.get(i + 8).setDouble(rotations);
+
+            // The absolute position of the swerve wheels.  We need the displacement offsets
+            // in order to force the wheels into the angles we actually want.
+            double currentAbsoluteAngle = dutyCycles.get(i).getOutput() * 360;
+            double displacementAngleDegrees = Constants.DISPLACEMENT_ANGLES[i];
+            currentAbsoluteAngle = (currentAbsoluteAngle - (displacementAngleDegrees - 180)) % 360 ;
+            entries.get(i + 4).setDouble(currentAbsoluteAngle);
+
+            // Tells us the distance between our desired angle from the controller and our current pivot motor angle, in degrees.
+            double deltaDegrees = pidControllers.get(i).calculate(currentAbsoluteAngle, rotations);
+            entries.get(i + 0).setDouble(deltaDegrees);
+
+            if (!pidControllers.get(i).atSetpoint()) {
+                // Percent of the distance we want to rotate relative to our desired degrees in this loop
+                final double MAX_TURNING_RATE = 1.0;
+               pivotMotors.get(i).set((deltaDegrees / 180) * MAX_TURNING_RATE);
+            }
         }
     }
 }
